f0787149b72aae064131fdb8ad3518361504b2a1
Donato Wolfisberg <donato@wolfisberg.dev>
2021-03-22 20:01:22 +0100

    just create config file if not exists

--- install.sh
+++ install.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env sh
 
-if [ ! -f /tmp/foo.txt ]; then
+CONFIG_FILE=/etc/server-updater/server-updater.env
+BINARY_FILE=/usr/local/bin/server-updater
+
+if [ -f "$CONFIG_FILE" ]; then
+    echo "config file at ${CONFIG_FILE} already exists not creating one"
+else 
     sudo mkdir -p /etc/server-updater
-    cat << EOF | sudo tee /etc/server-updater/server-updater.env > /dev/null
+    cat << EOF | sudo tee "$CONFIG_FILE" > /dev/null
     WEBHOOK_URL_INFO=
     WEBHOOK_URL_ERROR=
-    EOF
-    sudo chmod 006 /etc/server-updater/server-updater.env
+EOF
+    sudo chmod 006 "${CONFIG_FILE}"
 
-    echo "created config file at /etc/server-updater/server-updater.env"
+    echo "created config file at ${CONFIG_FILE}"
 fi
+
 
 LATEST_VERSION="$(curl -sL https://github.com/0xFEEDC0DE-dev/server-updater/releases \
                 | grep -o 'releases/tag/[0-9]*.[0-9]*.[0-9]*' \
                 | sort --version-sort \
                 | tail -1 \
-                | awk -F'/' '{ print $3}')"
+                | awk -F'/' '{ print $3}' \
+)"
 
-sudo wget -q -O /usr/local/bin/server-updater "https://github.com/0xFEEDC0DE-dev/server-updater/releases/download/${LATEST_VERSION}/server-updater_${LATEST_VERSION}_linux_amd64 2>&1 /dev/null
-sudo chmod 007 /usr/local/bin/server-updater
+sudo wget -q -O "${BINARY_FILE}" "https://github.com/0xFEEDC0DE-dev/server-updater/releases/download/${LATEST_VERSION}/server-updater_${LATEST_VERSION}_linux_amd64 2>&1 /dev/null
+sudo chmod 007 "${BINARY_FILE}"
 
-echo "0 2 * * * mkdir -p /tmp/server-updater && /usr/local/bin/server-updater --env-file /etc/server-updater/server-updater.env >> /tmp/server-updater/out.log 2>> /tmp/server-updater/err.log" | sudo crontab -u root -
+echo "0 2 * * * mkdir -p /tmp/server-updater && ${BINARY_FILE} --env-file ${CONFIG_FILE} >> /tmp/server-updater/out.log 2>> /tmp/server-updater/err.log" | sudo crontab -u root -
 
-echo "Downloaded binary version ${LATEST_VERSION} to /usr/local/bin/server-updater"
+echo "Downloaded binary version ${LATEST_VERSION} to ${BINARY_FILE}"
 echo "Created Cronjob to start every night"
