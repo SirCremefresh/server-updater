8f9b636028e29316fda214e7e4bf42ef4f0fcb66
Donato Wolfisberg <donato@wolfisberg.dev>
2021-03-22 19:39:08 +0100

    added chmod to install script

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ git ls-remote --tags origin | awk '/^(.*)(s+)(.*[a-zA-Z0-9])$/ {print ":" $2}' |
 # Install
 ```shell
 curl -fsSL https://raw.githubusercontent.com/0xFEEDC0DE-dev/server-updater/master/install.sh | sh
+# Set the webhooks in the config file
 sudo vim /etc/server-updater/server-updater.env
 
+# enable the cron service so that the crontab from the install script is run
 sudo systemctl enable cron
 ```
--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 sudo mkdir -p /etc/server-updater
-
 cat << EOF | sudo tee /etc/server-updater/server-updater.env > /dev/null
 WEBHOOK_URL_INFO=
 WEBHOOK_URL_ERROR=
 EOF
+sudo chmod 006 /etc/server-updater/server-updater.env
 
 sudo wget -q -O /usr/local/bin/server-updater https://github.com/0xFEEDC0DE-dev/server-updater/releases/download/0.0.1/server-updater_0.0.1_linux_amd64 2>&1 /dev/null
-sudo chmod +x /usr/local/bin/server-updater
+sudo chmod 007 /usr/local/bin/server-updater
 
 echo "0 2 * * * mkdir -p /tmp/server-updater && /usr/local/bin/server-updater --env-file /etc/server-updater/server-updater.env >> /tmp/server-updater/out.log 2>> /tmp/server-updater/err.log" | sudo crontab -u root -
 
