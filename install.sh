d9e836079240c87d9831474d75af5f2df01c0ba7
Donato Wolfisberg <donato@wolfisberg.dev>
2021-03-22 19:48:29 +0100

    getting latest version in install script

--- install.sh
+++ install.sh
@@ -7,11 +7,17 @@ WEBHOOK_URL_ERROR=
 EOF
 sudo chmod 006 /etc/server-updater/server-updater.env
 
-sudo wget -q -O /usr/local/bin/server-updater https://github.com/0xFEEDC0DE-dev/server-updater/releases/download/0.0.1/server-updater_0.0.1_linux_amd64 2>&1 /dev/null
+LATEST_VERSION="$(curl -sL https://github.com/0xFEEDC0DE-dev/server-updater/releases \
+                | grep -o 'releases/tag/[0-9]*.[0-9]*.[0-9]*' \
+                | sort --version-sort \
+                | tail -1 \
+                | awk -F'/' '{ print $3}')"
+
+sudo wget -q -O /usr/local/bin/server-updater "https://github.com/0xFEEDC0DE-dev/server-updater/releases/download/${LATEST_VERSION}/server-updater_${LATEST_VERSION}_linux_amd64 2>&1 /dev/null
 sudo chmod 007 /usr/local/bin/server-updater
 
 echo "0 2 * * * mkdir -p /tmp/server-updater && /usr/local/bin/server-updater --env-file /etc/server-updater/server-updater.env >> /tmp/server-updater/out.log 2>> /tmp/server-updater/err.log" | sudo crontab -u root -
 
-echo "Downloaded binary to /usr/local/bin/server-updater"
+echo "Downloaded binary version ${LATEST_VERSION} to /usr/local/bin/server-updater"
 echo "Put config file at /etc/server-updater/server-updater.env"
 echo "Created Cronjob to start every night"
